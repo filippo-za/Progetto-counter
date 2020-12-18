d5e5a7ef32acab0904b6730146784ab67455903c
Filippo Zarba <filippozarba00@gmail.com>
2020-12-18 23:15:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let add = document.querySelector(".btn_increment");
+
+let remove = document.querySelector(".btn_decrement");
+
+let numInt = document.querySelector(".num");
+let int = 0;
+
+add.addEventListener("click", function () {
+  int += 1;
+  numInt.innerHTML = int;
+});
